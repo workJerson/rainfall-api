6c2e7eadcbf1cee48935b3d7e520c1e20b365c56
Jerson <work.jerson.baltazar@gmail.com>
2024-04-22 07:51:23 +0800

    chore: Added static logger config (SeriLog)

---
+++ rainfall-api/Common/StaticLogger.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+using Serilog.Sinks.RollingFileAlternate;
+
+namespace rainfall_api.Common
+{
+    public static class StaticLogger
+    {
+        public static void EnsureInitialized()
+        {
+            if (Log.Logger is not Serilog.Core.Logger)
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.RollingFileAlternate(".\\logs")
+                    .WriteTo.Debug()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger();
+            }
+        }
+    }
+}
