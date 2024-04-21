e422701d9488c922a4532bea61ddc5ac0c603b6d
Jerson <work.jerson.baltazar@gmail.com>
2024-04-22 07:51:54 +0800

    chore: Added request/response logging middleware

---
+++ rainfall-api/Middlewares/ResponseLoggingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+using Serilog;
+
+namespace rainfall_api.Middlewares
+{
+    public class ResponseLoggingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
+        {
+            await next(httpContext);
+            var originalBody = httpContext.Response.Body;
+            using var newBody = new MemoryStream();
+            httpContext.Response.Body = newBody;
+            newBody.Seek(0, SeekOrigin.Begin);
+            string responseBody = await new StreamReader(httpContext.Response.Body).ReadToEndAsync();
+
+            LogContext.PushProperty("StatusCode", httpContext.Response.StatusCode);
+            LogContext.PushProperty("ResponseTimeUTC", DateTime.Now);
+            Log.ForContext("ResponseHeaders", httpContext.Response.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()), destructureObjects: true)
+           .ForContext("ResponseBody", responseBody)
+           .Information("HTTP {RequestMethod} Request to {RequestPath} by {RequesterEmail} has Status Code {StatusCode}.", httpContext.Request.Method, httpContext.Request.Path, "Anonymous", httpContext.Response.StatusCode);
+            newBody.Seek(0, SeekOrigin.Begin);
+            await newBody.CopyToAsync(originalBody);
+        }
+    }
+}
