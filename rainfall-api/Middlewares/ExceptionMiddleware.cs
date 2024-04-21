44d0afda17b7de3a14d48e1cbf8d35a701e37941
Jerson <work.jerson.baltazar@gmail.com>
2024-04-22 07:51:37 +0800

    chore: Added exception middleware

---
+++ rainfall-api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,76 @@
+﻿
+using rainfall_api.Common.Exceptions;
+using Serilog.Context;
+using Serilog;
+using System.Net;
+using rainfall_api.Dtos;
+using Newtonsoft.Json;
+
+namespace rainfall_api.Middlewares
+{
+    public class ExceptionMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception exception)
+            {
+                string errorId = Guid.NewGuid().ToString();
+                LogContext.PushProperty("ErrorId", errorId);
+                LogContext.PushProperty("StackTrace", exception.StackTrace);
+                var errorResult = new ErrorResponseModel
+                {
+                    Source = exception.TargetSite?.DeclaringType?.FullName,
+                    Exception = exception.Message.Trim(),
+                    ErrorId = errorId,
+                    SupportMessage = string.Format("Provide the ErrorId {0} to the support team for further analysis.", errorId)
+                };
+                errorResult.Messages.Add(exception.Message);
+                if (exception is not CustomException && exception.InnerException != null)
+                {
+                    while (exception.InnerException != null)
+                    {
+                        exception = exception.InnerException;
+                    }
+                }
+
+                switch (exception)
+                {
+                    case CustomException e:
+                        errorResult.StatusCode = (int)e.StatusCode;
+                        if (e.ErrorMessages is not null)
+                        {
+                            errorResult.Messages = e.ErrorMessages;
+                        }
+
+                        break;
+
+                    case KeyNotFoundException:
+                        errorResult.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    default:
+                        errorResult.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                Log.Error($"{errorResult.Exception} Request failed with Status Code {context.Response.StatusCode} and Error Id {errorId}.");
+                var response = context.Response;
+                if (!response.HasStarted)
+                {
+                    response.ContentType = "application/json";
+                    response.StatusCode = errorResult.StatusCode;
+                    errorResult.Source = "";
+                    await response.WriteAsync(JsonConvert.SerializeObject(errorResult));
+                }
+                else
+                {
+                    Log.Warning("Can't write error response. Response has already started.");
+                }
+            }
+        }
+    }
+}
