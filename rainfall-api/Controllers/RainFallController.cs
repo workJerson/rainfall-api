400fbc5489951d1ce0e8d4cbef66caa00801c61b
Jerson <work.jerson.baltazar@gmail.com>
2024-04-22 07:52:37 +0800

    feat: Added controller for rainfall

---
+++ rainfall-api/Controllers/RainFallController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using rainfall_api.Controllers.Common;
+using rainfall_api.Dtos;
+using rainfall_api.Services;
+
+namespace rainfall_api.Controllers
+{
+    public class RainFallController : BaseApiController
+    {
+        [HttpGet("id/{stationId}/readings")]
+        public async Task<RainfallReadingResponseModel> GetRainfallReadings([FromRoute] string stationId, [FromQuery] int? count = 10)
+        {
+            return await Mediator.Send(new GetRainfallReadingsRequest(stationId, count));
+        }
+    }
+}
