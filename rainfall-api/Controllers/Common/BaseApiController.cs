34132f4ab379fa0a1ac6be26ae98e97e6d2f0b28
Jerson <work.jerson.baltazar@gmail.com>
2024-04-22 07:52:11 +0800

    chore: Added base controller

---
+++ rainfall-api/Controllers/Common/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace rainfall_api.Controllers.Common
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        private ISender _mediator = null!;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
