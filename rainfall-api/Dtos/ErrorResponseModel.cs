857e5e4f05db89be44e030e09ef3f800a472c929
workJerson <work.jerson.baltazar@gmail.com>
2024-04-22 10:16:58 +0800

    chore: Added new class for error details

--- rainfall-api/Dtos/ErrorResponseModel.cs
+++ rainfall-api/Dtos/ErrorResponseModel.cs
@@ -9,4 +9,12 @@
         public string? SupportMessage { get; set; }
         public int StatusCode { get; set; }
     }
+    /// <summary>
+    /// Details of invalid request property
+    /// </summary>
+    public class ErrorDetailModel
+    {
+        public string PropertyName { get; set; }
+        public string Message { get; set; }
+    }
 }
